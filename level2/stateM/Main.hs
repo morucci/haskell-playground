c569ad4a45efab8711d14a7e753144a1fed1ac78
Fabien Boucher <fboucher@redhat.com>
2021-09-19 20:14:08 +0000

    Use stateT

--- level2/stateM/Main.hs
+++ level2/stateM/Main.hs
@@ -19,30 +19,56 @@ import Control.Monad.State.Lazy
 -- runState stateIntString 1
 ------------
 
+-- tick :: State Int Int
+-- tick = do
+--   n <- get
+--   put (n + 1)
+--   return n
+
 data GameState = GameState {gsSecret :: Int, gsWon :: Bool, gsGuessAttempt :: Int, gsAttempts :: [Int]} deriving (Show)
 
-checkGuess :: Int -> State GameState Bool
+checkGuess :: Int -> State GameState Int
 checkGuess guess = do
   s <- get
   let won = gsSecret s == guess
       pa = guess : gsAttempts s
   put $ GameState (gsSecret s) won (gsGuessAttempt s + 1) pa
-  pure won
+  pure guess
 
--- λ> import Main
 -- λ> runState (checkGuess 2) $ GameState 5 False 0
 
-guessGame :: State GameState Bool
+-- With the state monad
+guessGame :: State GameState Int
 guessGame = do
   checkGuess 1
   checkGuess 2
   checkGuess 5
 
-tick :: State Int Int
-tick = do
-  n <- get
-  put (n + 1)
-  return n
+-- λ> runState guessGame $ GameState 5 False 0 []
+
+type AppM = StateT GameState IO ()
+
+runGame :: AppM
+runGame = game
+  where
+    game :: AppM
+    game = do
+      s <- get
+      if not $ gsWon s
+        then round >> game
+        else do
+          liftIO . print $ "Game ended with state:" <> show s
+          pure ()
+    round :: AppM
+    round = do
+      liftIO $ putStrLn "Enter a guess"
+      guessS <- liftIO getLine
+      let guess = read guessS :: Int
+      s <- get
+      let won = gsSecret s == guess
+          pa = guess : gsAttempts s
+      put $ GameState (gsSecret s) won (gsGuessAttempt s + 1) pa
+      pure ()
 
 main :: IO ()
-main = print ""
+main = print "Test"
